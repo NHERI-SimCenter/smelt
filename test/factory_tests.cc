8206f17613c44d6e058bb5f85a24d111b8c96983
Michael Gardner <shellshocked2003@gmail.com>
2018-10-09 15:07:59 -0700

    Added unit tests for factory, will add for multivariate normal distribution next

---
+++ test/factory_tests.cc
@@ -0,0 +1,99 @@
+#include <iostream>
+#include <catch/catch.hpp>
+#include <Eigen/Dense>
+#include "factory.h"
+#include "normal_multivar.h"
+#include "numeric_utils.h"
+
+class BaseClass {
+public:
+  // Default constructor
+  BaseClass() = default;
+  // Get class name
+  virtual std::string name() const = 0;
+};
+
+class DerivedClass : public BaseClass {
+public:
+  // Default constructor
+  DerivedClass() = default;
+  // Get class name
+  virtual std::string name() const { return "DerivedClass"; };
+};
+
+class AnotherDerivedClass : public BaseClass {
+public:
+  // Default constructor
+  AnotherDerivedClass() = default;
+  // Get class name
+  virtual std::string name() const { return "AnotherDerivedClass"; };
+};
+
+namespace numeric_utils {
+class TestingClass : public RandomGenerator {
+public:
+  TestingClass() : RandomGenerator() {};
+
+  TestingClass(int seed) : RandomGenerator(){};
+
+  virtual ~TestingClass(){};
+
+  Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic>
+  generate(const Eigen::VectorXd &means, const Eigen::MatrixXd &cov,
+           unsigned int cases = 1) override {
+    return Eigen::MatrixXd::Zero(2, 2);
+  };
+  
+  std::string name() const override {
+    return "TestingClass";
+  };  
+};  
+}
+
+
+/**
+ * Check the factory class implementation
+ */
+TEST_CASE("Check the factory class implementation", "[Factory]") {
+  // Seed value for repeatability
+  int seed = 100;
+  
+  SECTION("Create factory at compile time") {
+    // Register classes
+    static Register<BaseClass, DerivedClass> derived("DerivedClass");
+    static Register<BaseClass, AnotherDerivedClass> another(
+        "AnotherDerivedClass");
+
+    // Create derived class
+    REQUIRE(Factory<BaseClass>::instance()->check("DerivedClass") == true);
+    auto derived_class = Factory<BaseClass>::instance()->create("DerivedClass");
+    REQUIRE(derived_class->name() == "DerivedClass");
+
+    // Create another derived class
+    REQUIRE(Factory<BaseClass>::instance()->check("AnotherDerivedClass") ==
+            true);
+    auto another_derived_class =
+        Factory<BaseClass>::instance()->create("AnotherDerivedClass");
+    REQUIRE(another_derived_class->name() == "AnotherDerivedClass");
+
+    // Create a non-existant element
+    REQUIRE(Factory<BaseClass>::instance()->check("Garbage") == false);
+    try {
+      auto garbage = Factory<BaseClass>::instance()->create("Garbage");
+    } catch (std::exception &exception) {
+      std::cout << "Factory error: " << exception.what() << std::endl;
+    }
+  }
+
+  SECTION("Extend library when linked") {
+    // Register class with factory
+    Register<numeric_utils::RandomGenerator, numeric_utils::TestingClass, int>
+        testing_multivar("TestingClass");
+
+    // Create newly registered derived class
+    REQUIRE(Factory<numeric_utils::RandomGenerator, int>::instance()->check("TestingClass") == true);
+    auto testing_class = Factory<numeric_utils::RandomGenerator, int>::instance()
+      ->create("TestingClass", std::move(seed));
+    REQUIRE(testing_class->name() == "TestingClass");    
+  }
+}
--- test/multi_var_tests.cc
+++ test/multi_var_tests.cc
@@ -1,11 +1,15 @@
 #include <catch/catch.hpp>
 #include <Eigen/Dense>
+#include "configure.h"
 #include "factory.h"
 #include "normal_multivar.h"
 #include "numeric_utils.h"
 
 TEST_CASE("Test generation of random numbers", "[RandomNumbers]") {
-  unsigned int seed = 100;
+  // Initialize the factories
+  config::initialize();
+  // Seed value for repeatability
+  int seed = 100;
   
   SECTION("Generate normally distributed random numbers for single random "
           "variable") {
@@ -13,9 +17,8 @@ TEST_CASE("Test generation of random numbers", "[RandomNumbers]") {
     Eigen::MatrixXd cov(1,1);    
     means(0) = 1.789;
     cov(0, 0) = 0.0123;
-
-    // Register<numeric_utils::RandomGenerator, numeric_utils::NormalMultiVar, int> normal_multivar("MultivariateNormal");
+    
     auto random_generator = Factory<numeric_utils::RandomGenerator, int>::instance()
-      ->create("MultivariateNormal", 100);
+      ->create("MultivariateNormal", std::move(seed));    
   }
 }
