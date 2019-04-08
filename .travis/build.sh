80dce1240a620a357e02ec04c15e1c4c569b164b
Michael Gardner <shellshocked2003@gmail.com>
2019-04-08 12:38:42 -0700

    Testing build script

--- .travis/build.sh
+++ .travis/build.sh
@@ -1,37 +1,43 @@
 #!/bin/bash
 
 if [[ "$TRAVIS_OS_NAME" == "osx" ]]
-  wget https://repo.anaconda.com/miniconda/Miniconda3-latest-MacOSX-x86_64.sh -O ~/miniconda.sh
-  bash ~/miniconda.sh -b -p $HOME/miniconda
-  export PATH="$HOME/miniconda/bin:$PATH"
-  conda update --yes conda
-  conda install -c intel mkl --yes
-  conda install -c intel mkl-include --yes
-  conda install -c intel ipp --yes
-  conda install -c intel ipp-include --yes
-  export MKLROOT=$HOME/miniconda
-  export IPPROOT=$HOME/miniconda
-  export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$HOME/miniconda/lib  
-  mkdir build
-  cd build
-  cmake ..
-  make
-  make test  
+   printf "\nHello from Mac\n"
 else
-  wget https://apt.repos.intel.com/intel-gpg-keys/GPG-PUB-KEY-INTEL-SW-PRODUCTS-2019.PUB
-  sudo apt-key add GPG-PUB-KEY-INTEL-SW-PRODUCTS-2019.PUB
-  sudo sh -c 'echo deb https://apt.repos.intel.com/mkl all main > /etc/apt/sources.list.d/intel-mkl.list'
-  sudo sh -c 'echo deb https://apt.repos.intel.com/ipp all main > /etc/apt/sources.list.d/intel-ipp.list'
-  sudo apt-get update
-  sudo apt-get install intel-mkl-2019.3-062
-  sudo apt-get install intel-ipp-2019.3-062
-  source /opt/intel/mkl/bin/mklvars.sh intel64
-  source /opt/intel/ipp/bin/ippvars.sh intel64 linux
-  mkdir build
-  cd build
-  cmake ..
-  make
-  make test
-  cd ..
-  doxygen Doxyfile
+   printf "\Hello from linux\n"
 fi
+
+# if [[ "$TRAVIS_OS_NAME" == "osx" ]]
+#   wget https://repo.anaconda.com/miniconda/Miniconda3-latest-MacOSX-x86_64.sh -O ~/miniconda.sh
+#   bash ~/miniconda.sh -b -p $HOME/miniconda
+#   export PATH="$HOME/miniconda/bin:$PATH"
+#   conda update --yes conda
+#   conda install -c intel mkl --yes
+#   conda install -c intel mkl-include --yes
+#   conda install -c intel ipp --yes
+#   conda install -c intel ipp-include --yes
+#   export MKLROOT=$HOME/miniconda
+#   export IPPROOT=$HOME/miniconda
+#   export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$HOME/miniconda/lib  
+#   mkdir build
+#   cd build
+#   cmake ..
+#   make
+#   make test  
+# else
+#   wget https://apt.repos.intel.com/intel-gpg-keys/GPG-PUB-KEY-INTEL-SW-PRODUCTS-2019.PUB
+#   sudo apt-key add GPG-PUB-KEY-INTEL-SW-PRODUCTS-2019.PUB
+#   sudo sh -c 'echo deb https://apt.repos.intel.com/mkl all main > /etc/apt/sources.list.d/intel-mkl.list'
+#   sudo sh -c 'echo deb https://apt.repos.intel.com/ipp all main > /etc/apt/sources.list.d/intel-ipp.list'
+#   sudo apt-get update
+#   sudo apt-get install intel-mkl-2019.3-062
+#   sudo apt-get install intel-ipp-2019.3-062
+#   source /opt/intel/mkl/bin/mklvars.sh intel64
+#   source /opt/intel/ipp/bin/ippvars.sh intel64 linux
+#   mkdir build
+#   cd build
+#   cmake ..
+#   make
+#   make test
+#   cd ..
+#   doxygen Doxyfile
+# fi
