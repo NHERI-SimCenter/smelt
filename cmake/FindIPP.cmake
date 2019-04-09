f9877a64d07bb49a9daf52e31bb636dba9ac0869
Michael Gardner <shellshocked2003@gmail.com>
2019-04-09 14:23:41 -0700

    Updated cmake files to search in different locations when Windows

--- cmake/FindIPP.cmake
+++ cmake/FindIPP.cmake
@@ -2,7 +2,8 @@
 
 set(IPP_LIB "ippcore" "ippvm" "ipps")
 
-function(ipp_libs)
+# For Linux and Mac
+function(ipp_libs_unix)
   foreach(s ${ARGV})
     find_library(${s}_LIBRARY
       NAMES ${s}
@@ -22,4 +23,33 @@ function(ipp_libs)
 
 endfunction()
 
-ipp_libs(${IPP_LIB})
+# For Windows. NOTE: Assumes conda was used to install IPP
+function(ipp_libs_windows)
+  foreach(s ${ARGV})
+    find_library(${s}_LIBRARY
+      NAMES ${s}
+      PATHS $ENV{IPPROOT}/lib
+      $ENV{IPPROOT}/lib/intel64
+      $ENV{IPPROOT}/../compiler/lib/intel64
+      $ENV{IPPROOT}/Library/bin      
+      NO_DEFAULT_PATH)
+    if(NOT ${s}_LIBRARY)
+      message(FATAL_ERROR "NOT FOUND: " ${s})
+    endif()
+  
+    list(APPEND IPP_FOUND_LIBRARIES ${${s}_LIBRARY})
+  endforeach()
+ 
+  set(IPP_INCLUDE_DIRS $ENV{IPPROOT}/include PARENT_SCOPE)
+  set(IPP_LIBRARIES ${IPP_FOUND_LIBRARIES} PARENT_SCOPE)
+
+endfunction()
+
+
+if(WIN32)
+ ipp_libs_windows(${IPP_LIB})  
+endif()
+
+if(NOT WIN32)
+  ipp_libs_unix(${IPP_LIB})
+endif()
