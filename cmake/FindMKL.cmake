752ad371c670ba82c33e750d763aa96d36a48351
Michael Gardner <shellshocked2003@gmail.com>
2019-08-12 15:53:43 -0700

    Trying sequential build

--- cmake/FindMKL.cmake
+++ cmake/FindMKL.cmake
@@ -1,11 +1,21 @@
 include(HelperFunctions)
 # Find the Math Kernel Library from Intel
 
+# if (BUILD_STATIC_LIBS)
+#   if (APPLE)
+#     set(MKL_LIB "mkl_intel_lp64" "mkl_tbb_thread" "mkl_core")
+#   else()
+#     set(MKL_LIB "mkl_intel_lp64" "mkl_tbb_thread" "mkl_core")    
+#   endif()
+  
+#   add_mkl_dependency(NAME mkl_static REQD_LIBS ${MKL_LIB} STATIC)
+# endif()
+
 if (BUILD_STATIC_LIBS)
   if (APPLE)
-    set(MKL_LIB "mkl_intel_lp64" "mkl_tbb_thread" "mkl_core")
+    set(MKL_LIB "mkl_intel_lp64" "mkl_sequential" "mkl_core")
   else()
-    set(MKL_LIB "mkl_intel_lp64" "mkl_tbb_thread" "mkl_core")    
+    set(MKL_LIB "mkl_intel_lp64" "mkl_sequential" "mkl_core")    
   endif()
   
   add_mkl_dependency(NAME mkl_static REQD_LIBS ${MKL_LIB} STATIC)
