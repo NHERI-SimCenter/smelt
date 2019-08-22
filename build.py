bebf3e5f2b06c3026b244eb0036966f32db935ea
Michael Gardner <shellshocked2003@gmail.com>
2019-08-22 08:50:02 -0700

    Changed how linker flags are passed

--- build.py
+++ build.py
@@ -6,8 +6,8 @@ from bincrafters import build_template_default
 
 if __name__ == "__main__":
 
-    subprocess.run(["conan", "profile", "new", "default", "--detect"])
-    subprocess.run(["conan", "profile", "update", "settings.compiler.libcxx=libstdc++11", "default"])    
+    # subprocess.run(["conan", "profile", "new", "default", "--detect"])
+    # subprocess.run(["conan", "profile", "update", "settings.compiler.libcxx=libstdc++11", "default"])    
     
     builder = build_template_default.get_builder(build_types=["Release", "Debug"], archs=["x86_64"])
     builder.run()
