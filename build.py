51bcdaaaa6b8dbb159d502cf5aea9ff3e64393bb
Michael Gardner <shellshocked2003@gmail.com>
2019-08-22 08:13:27 -0700

    Update compiler settings in build.py and add sudo for linux machines

--- build.py
+++ build.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-
+import subprocess
 from bincrafters import build_template_default
 
 if __name__ == "__main__":
 
+    subprocess.run(["conan", "profile", "new", "default", "--detect"])
+    subprocess.run(["conan", "profile", "update", "settings.compiler.libcxx=libstdc++11", "default"])    
+    
     builder = build_template_default.get_builder(build_types=["Release", "Debug"], archs=["x86_64"])
     builder.run()
