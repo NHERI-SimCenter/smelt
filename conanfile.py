bb3c963f353c427016484533130f137473f4c0d4
Michael Gardner <shellshocked2003@gmail.com>
2019-08-22 10:54:54 -0700

    Added build dir for tests in cmake in conanfile

--- conanfile.py
+++ conanfile.py
@@ -44,7 +44,7 @@ class smeltConan(ConanFile):
     def build(self):
         cmake = self.configure_cmake()
         cmake.build()
-        cmake.test()
+        cmake.test("bin")
 
     def build_id(self):
         self.info_build.settings.build_type = "Any"            
