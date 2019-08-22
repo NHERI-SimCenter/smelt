14279bb973f92f87b1342a3bbd0ce6b2dafaf691
Michael Gardner <shellshocked2003@gmail.com>
2019-08-22 08:08:21 -0700

    Removed specification of compiler in builder

--- build.py
+++ build.py
@@ -6,5 +6,5 @@ from bincrafters import build_template_default
 
 if __name__ == "__main__":
 
-    builder = build_template_default.get_builder(build_types=["Release", "Debug"], archs=["x86_64"], gcc_versions=["7"], clang_versions=[])
+    builder = build_template_default.get_builder(build_types=["Release", "Debug"], archs=["x86_64"])
     builder.run()
