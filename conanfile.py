5a2229500ea5a8c6c934ce0e419e667eb97a9ed4
Michael Gardner <shellshocked2003@gmail.com>
2019-08-22 11:32:25 -0700

    Changed imports to place all shared libs in bin folder

--- conanfile.py
+++ conanfile.py
@@ -28,8 +28,8 @@ class smeltConan(ConanFile):
 
     def imports(self):
         self.copy("*.dll", "", "bin")
-        self.copy("*.dylib", "", "lib")
-        self.copy("*.so", "", "lib")
+        self.copy("*.dylib", "", "bin")
+        self.copy("*.so", "", "bin")
         
  # bin, *.dll -> ./bin # Copies all dll files from packages bin folder to my "bin" folder
  # lib, *.dylib* -> ./bin # Copies all dylib files from packages lib folder to my "bin" folder
