fc25dd60fe70881575cdc784e50c9a175cb8e7d8
Michael Gardner <shellshocked2003@gmail.com>
2019-08-26 13:03:02 -0700

    Updated IPP library names for static Windows build

--- cmake/FindIPP.cmake
+++ cmake/FindIPP.cmake
@@ -4,7 +4,13 @@ include(HelperFunctions)
 set(IPP_LIB "ippcore" "ippvm" "ipps")
 
 if (BUILD_STATIC_LIBS)
-  add_ipp_dependency(NAME ipp_static REQD_LIBS ${IPP_LIB} STATIC)
+  if (WIN32)
+    set(IPP_LIB_WIN "ippcoremt" "ippvmmt" "ippsmt")
+    add_ipp_dependency(NAME ipp_static REQD_LIBS ${IPP_LIB_WIN} STATIC)
+  else()
+    add_ipp_dependency(NAME ipp_static REQD_LIBS ${IPP_LIB} STATIC)    
+  endif()
+    
 endif()
 
 if (BUILD_SHARED_LIBS)
